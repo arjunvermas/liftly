e31e2f6529e0143e9a91c42b22bda0e19ea9f40e
arjunvermas <arjunv12345690@gmail.com>
2026-09-20 11:04:52 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const dotenv = require('dotenv')
+dotenv.config();
+const app = require('./app.js');
+
+
+
+const port = process.env.PORT;
+app.listen(port, () => console.log("server is up"))
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,6 @@
+const bcrypt = require("bcrypt");
+const supabase = require("../db");
+
+async function registerUser(req, res){
+
+}
